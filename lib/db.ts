51b264c9e2982e4da50695d0471d2fbd12977fed
surajxcelerate-star <suraj.xcelerate@gmail.com>
2026-06-07 13:27:10 +0545

    Fixed db

--- lib/db.ts
+++ lib/db.ts
@@ -1,14 +1,19 @@
 import mysql from "mysql2/promise";
 
 const pool = mysql.createPool({
-  host:"gateway01.ap-southeast-1.prod.alicloud.tidbcloud.com",
-  port: parseInt("4000"),
+  host: "gateway01.ap-southeast-1.prod.alicloud.tidbcloud.com",
+  port: 4000,
   user: "42ozeHBxPnrXopQ.root",
   password: "6M3sQetP34KH35bl",
   database: "hotel_ordering",
+
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
+
+  ssl: {
+    rejectUnauthorized: true,
+  },
 });
 
 export default pool;
